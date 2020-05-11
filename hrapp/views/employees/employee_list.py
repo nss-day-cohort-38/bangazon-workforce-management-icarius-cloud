fb0e7e0ae7b846277e893dd9b5f4be0b5be6a25e
MatthewScott32 <matthewscottblagg@gmail.com>
2020-05-11 12:21:10 -0500

    employee list with department working

--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -1,23 +1,27 @@
 import sqlite3
 from django.shortcuts import render
-from hrapp.models import Employee
+from hrapp.models import Employee, Department
+from ..connection import Connection
 
 
 def employee_list(request):
     if request.method == 'GET':
-        with sqlite3.connect("") as conn:
+        with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
 
             # TODO: Add to query: e.department,
             db_cursor.execute("""
-            select
+           SELECT
                 e.id,
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.is_supervisor
-            from hrapp_employee e
+                e.is_supervisor,
+                d.name
+            FROM hrapp_employee e
+            JOIN hrapp_department d               
+            ON e.department_id = d.id
             """)
 
             all_employees = []
@@ -30,7 +34,7 @@ def employee_list(request):
                 employee.last_name = row['last_name']
                 employee.start_date = row['start_date']
                 employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
+                employee.name = row['name']
 
                 all_employees.append(employee)
 
