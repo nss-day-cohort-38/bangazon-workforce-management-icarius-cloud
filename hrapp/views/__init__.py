afbcf8c557a626a50c8c0551601fa828293d9b0b
mKroogz <mattk932009@gmail.com>
2020-05-11 14:55:34 -0500

    can list and detail comps, added if to template to check for decommission for dynamic render

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -1,3 +1,5 @@
 from .employees.employee_list import employee_list
+from .computers.comp_list import computer_list
+from .computers.comp_details import computer_details
 from .home import home
 from .auth.logout import logout_user
