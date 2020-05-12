41742e91f21a7d33d7762210beaaef62005e51e3
sncandiani <sofiancandiani@gmail.com>
2020-05-12 14:49:05 -0500

    User can view associated employees with training program

--- hrapp/views/training_programs/detail.py
+++ hrapp/views/training_programs/detail.py
@@ -21,14 +21,38 @@ def get_training_program(training_program_id):
 
         return db_cursor.fetchone()
 
+def get_training_program_employees(training_program_id): 
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(TrainingProgram)
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+             etp.id, 
+             etp.employee_id,
+             etp.training_program_id, 
+             e.id, 
+             e.first_name, 
+             e.last_name, 
+             tp.id, 
+             tp.title
+             FROM hrapp_employeetrainingprogram AS etp
+             JOIN hrapp_trainingprogram AS tp
+             ON tp.id = etp.training_program_id
+             JOIN hrapp_employee AS e
+             ON e.id = etp.employee_id
+             WHERE tp.id = ?    
+        """, (training_program_id,))
+        return db_cursor.fetchall()
 
 
 def training_program_details(request, training_program_id):
     if(request.method == 'GET'):
         training_program = get_training_program(training_program_id)
+        employees = get_training_program_employees(training_program_id)
         template = 'training_programs/training_program_detail.html'
         context = {
-            'training_program': training_program
+            'training_program': training_program, 
+            'employees': employees
         }
         return render(request, template, context)
     elif request.method == 'POST':
