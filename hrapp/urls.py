acb93c05023c21b00232b72c8b2f57e589f2383a
Jeremy Mattingly <jamattingly@live.com>
2020-05-12 11:07:11 -0500

    employee details working

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
+    path('employees/<int:employee_id>/', employee_details, name='employee_details'),
     path('departments/', department_list, name='department_list'),
     path('departments/form', department_form, name='department_form'),
     path('departments/<str:department_name>/', department_details, name='department_details'),
