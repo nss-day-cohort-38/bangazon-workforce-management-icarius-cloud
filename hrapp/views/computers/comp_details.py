a27a43eb0fb2d3c5e4acd79a1c6f86ad899abb34
mKroogz <mattk932009@gmail.com>
2020-05-12 21:10:00 -0500

    added decommission functionality

--- hrapp/views/computers/comp_details.py
+++ hrapp/views/computers/comp_details.py
@@ -1,5 +1,6 @@
 import sqlite3
 from django.urls import reverse
+from datetime import datetime
 from django.shortcuts import render, redirect
 from hrapp.models import model_factory, Computer
 from ..connection import Connection
@@ -24,6 +25,30 @@ def get_computer(computer_id):
         return db_cursor.fetchone()
 
 
+def decommission(computer_id):
+    now = datetime.now()
+
+    with sqlite3.connect(Connection.db_path) as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+                UPDATE hrapp_computer
+                SET decommission_date = ?
+                WHERE id = ?
+                """,
+                          (
+                              now.strftime("%Y-%m-%d"), computer_id,
+                          ))
+
+def remove_emp_connection(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+            DELETE FROM hrapp_employeecomputer
+            WHERE id = ?
+            """, (computer_id,))
+
 def computer_details(request, computer_id):
     if request.method == 'GET':
         computer = get_computer(computer_id)
@@ -34,3 +59,16 @@ def computer_details(request, computer_id):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            decommission(computer_id)
+            remove_emp_connection(computer_id)
+
+        return redirect(reverse('hrapp:computers'))
+
